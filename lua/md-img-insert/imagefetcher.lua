8cf261d886e776881495a29a33191cd82ef2dba5
Junayd Bhoyroo <contact@junaydb.com>
2024-10-31 23:02:21 +0000

    style: format

--- lua/md-img-insert/imagefetcher.lua
+++ lua/md-img-insert/imagefetcher.lua
@@ -1,59 +1,59 @@
-local Job = require("plenary.job")
+local Job = require('plenary.job')
 
 local M = {}
 
 function M.fetch(use_cache, config, success_cb, error_cb, set_job)
-	if use_cache and vim.fn.filereadable(config.cache_path) == 1 then
-		vim.schedule(function()
-			success_cb(vim.fn.json_decode(vim.fn.readfile(config.cache_path)))
-		end)
+  if use_cache and vim.fn.filereadable(config.cache_path) == 1 then
+    vim.schedule(function()
+      success_cb(vim.fn.json_decode(vim.fn.readfile(config.cache_path)))
+    end)
 
-		print("Used cache (" .. config.cache_path .. ")")
+    print('Used cache (' .. config.cache_path .. ')')
 
-		return
-	end
+    return
+  end
 
-	local fetch_job = nil
+  local fetch_job = nil
 
-	fetch_job = Job:new({
-		command = "curl",
-		args = {
-			"https://api.imagekit.io/v1/files",
-			"-H",
-			"Accept: application/json",
-			"-u",
-			config.api_key .. ":",
-		},
-		-- TODO: Fix error handling
-		-- on_stderr = function(err)
-		-- 	vim.schedule(function()
-		-- 		error_cb({ "Error:", err })
-		-- 	end)
-		-- end,
-		on_exit = function(self, _, signal)
-			if signal == 2 then
-				return
-			end
+  fetch_job = Job:new({
+    command = 'curl',
+    args = {
+      'https://api.imagekit.io/v1/files',
+      '-H',
+      'Accept: application/json',
+      '-u',
+      config.api_key .. ':',
+    },
+    -- TODO: Fix error handling
+    -- on_stderr = function(err)
+    -- 	vim.schedule(function()
+    -- 		error_cb({ "Error:", err })
+    -- 	end)
+    -- end,
+    on_exit = function(self, _, signal)
+      if signal == 2 then
+        return
+      end
 
-			local result = self:result()
+      local result = self:result()
 
-			vim.schedule(function()
-				success_cb(vim.fn.json_decode(result))
-			end)
+      vim.schedule(function()
+        success_cb(vim.fn.json_decode(result))
+      end)
 
-			vim.schedule(function()
-				vim.fn.writefile(result, config.cache_path)
-				print("Cache written to (" .. config.cache_path .. ")")
-			end)
+      vim.schedule(function()
+        vim.fn.writefile(result, config.cache_path)
+        print('Cache written to (' .. config.cache_path .. ')')
+      end)
 
-			fetch_job = nil
-			set_job(nil)
-		end,
-	})
+      fetch_job = nil
+      set_job(nil)
+    end,
+  })
 
-	fetch_job:start()
+  fetch_job:start()
 
-	set_job(fetch_job)
+  set_job(fetch_job)
 end
 
 return M
--- lua/md-img-insert/init.lua
+++ lua/md-img-insert/init.lua
@@ -1,27 +1,27 @@
-local ui = require("md-img-insert.ui")
+local ui = require('md-img-insert.ui')
 
 local M = {}
 
 M.config = {
-	base_url_name = "MD_IMG_INSERT_BASE_URL",
-	api_key_name = "MD_IMG_INSERT_API_KEY",
-	cache_path = vim.fn.stdpath("data") .. "/markdown_images_cache.json",
-	window = {
-		border = "single",
-	},
+  base_url_name = 'MD_IMG_INSERT_BASE_URL',
+  api_key_name = 'MD_IMG_INSERT_API_KEY',
+  cache_path = vim.fn.stdpath('data') .. '/markdown_images_cache.json',
+  window = {
+    border = 'single',
+  },
 }
 
 function M.setup(opts)
-	M.config = vim.tbl_deep_extend("force", M.config, opts or {})
+  M.config = vim.tbl_deep_extend('force', M.config, opts or {})
 
-	M.config.base_url = os.getenv(M.config.base_url_name)
-	M.config.api_key = os.getenv(M.config.api_key_name)
+  M.config.base_url = os.getenv(M.config.base_url_name)
+  M.config.api_key = os.getenv(M.config.api_key_name)
 
-	vim.api.nvim_create_user_command("InsertMarkdownImage", function()
-		ui.create_tooltip_window(M.config)
-	end, {})
+  vim.api.nvim_create_user_command('InsertMarkdownImage', function()
+    ui.create_tooltip_window(M.config)
+  end, {})
 
-	vim.keymap.set("n", "<Leader>mi", ":InsertMarkdownImage<CR>", { noremap = true, desc = "Insert [m]arkdown [i]mage" })
+  vim.keymap.set('n', '<Leader>mi', ':InsertMarkdownImage<CR>', { noremap = true, desc = 'Insert [m]arkdown [i]mage' })
 end
 
 return M
